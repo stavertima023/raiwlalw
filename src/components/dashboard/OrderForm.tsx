9dcdd1a7a6c72930a43fbbd54126275632c9e18a
Yuk -_- <stavertima023@gmail.com>
2025-07-12 05:11:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/dashboard/OrderForm.tsx
+++ src/components/dashboard/OrderForm.tsx
@@ -32,7 +32,7 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { useToast } from '@/components/ui/use-toast';
+import { useToast } from '@/hooks/use-toast';
 import {
   OrderSchema,
   OrderStatusEnum,
