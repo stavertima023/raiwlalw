2795b7c5e8f234715e4286833b405df3e82f88fe
stavertima023 <stavertima023@gmail.com>
2025-07-15 21:02:58 +0700

    10

--- src/components/layout/UserNav.tsx
+++ src/components/layout/UserNav.tsx
@@ -25,7 +25,7 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt={user.name} />
+            {/* <AvatarImage src="/avatars/01.png" alt={user.name} /> */}
             <AvatarFallback>{user.name.charAt(0).toUpperCase()}</AvatarFallback>
           </Avatar>
         </Button>
